02c638dfffbe5397d463d96124d61d7d83c4582d
Bibek Pandit <bibekpandit.008@gmail.com>
2019-12-02 16:54:00 -0500

    added post request to app.js

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
+var bodyParser     =        require("body-parser");
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
- res.send('Alligator Approaches');
+    res.send('Alligator Approaches');
+});
+
+app.post('/login',function(req,res){
+    res.send('Received Your Post Request');
 });
 
 app.listen(port, function () {
- console.log('Example app listening on port !');
+    console.log('Example app listening on port !');
 });
