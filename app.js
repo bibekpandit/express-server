5a932599bbba2ca175cad288b5e0a22aeb271481
Bibek Pandit <bibekpandit.008@gmail.com>
2019-12-03 19:29:10 -0500

    Added data functionality

--- app.js
+++ app.js
@@ -1,18 +1,49 @@
 var express = require('express');
+var os = require("os");
+var bodyParser = require("body-parser");
+const path = require('path');
+var fs = require('fs')
+
 var port = process.env.PORT || 3000;
 var app = express();
-var bodyParser     =        require("body-parser");
+
+
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get('/', function (req, res) {
     res.send('Alligator Approaches');
 });
 
-app.post('/login',function(req,res){
+
+app.post('/post',function(req,res){
+    var x = req.body.x.split(",");
+    var y = req.body.y.split(",");
+    var z = req.body.z.split(",");
+    var state = req.body.state;
+    var noOfInstances = req.body.noOfInstances;
+
+    var logger = fs.createWriteStream('data.txt', {
+        flags: 'a' // 'a' means appending (old data will be preserved)
+      })
+
+    for (let i = 0; i < noOfInstances; i++){
+        logger.write(state + " " + x[i] + " " + y[i] + " " + z[i] + os.EOL);
+    }
+
+    logger.end()
+    console.log("Data Written!");
     res.send('Received Your Post Request');
+});
+
+
+
+app.get('/download', (req, res) => {
+    res.download(path.join(__dirname, "/data.txt"));
 });
 
 app.listen(port, function () {
