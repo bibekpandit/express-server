43989fed2d6ef6321e00910d5c530404ee60b07e
Bibek Pandit <bibekpandit.008@gmail.com>
2019-12-11 16:03:28 -0500

    Emptied data.txt

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.post('/post',function(req,res){
 
     logger.end()
     console.log("Data Written!");
-    res.send('Received Your Post Request');
+    res.send('Post Request Received');
 });
 
 
